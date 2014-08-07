eb7282c7aa09a08a75683553edaead0b09a6b3ce
smudd <smudd@574583bd-889e-4630-b937-5fec7977a2d0>
2014-08-07 08:04:54 +0000

    Building the CRUNCH_bins object

--- CRUNCH_bins.cpp
+++ CRUNCH_bins.cpp
@@ -13,6 +13,8 @@
 #include "CRN_parameters.hpp"
 #include "CRN_tParticle_bins.hpp"
 #include "CRUNCH_bins.hpp"
+#include "VolumeParticleInfo.hpp"
+#include "CRN_tParticle_bins.hpp"
 using namespace std;
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -22,5 +24,46 @@ void CRUNCH_bins::create()
 {
   cout << "You need to at least provide the number of bins and cells. " << endl;
 }
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// The create function:
+// this just has some information about the cell locations
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+void CRUNCH_bins::create(int n_pdz, int n_caz, VolumeParticleInfo this_vpi)
+{
+  n_pdz_cells = n_pdz;
+  n_caz_cells = n_caz;
+  vpi = ths_vpi;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// The create function:
+// this just has some information about the cell locations
+// the data is stored in vectors, you need to get the index into
+// the vector of from the bin and cell number
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+int CRUNCH_bins::retrieve_index_of_cell(int bin, int cell)
+{
+  int index_into_cell = (n_caz_cells+n_pdz_cell)*bin+cell;
+
+}
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// The create function:
+// this just has some information about the cell locations
+// the data is stored in vectors, you need to get the index into
+// the vector of from the bin and cell number
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+void CRUNCH_bins::populate_cells_with_geochemical_data_from_CRNtPb(
+                                                CRN_tParticle_bins& CRN_tPb);
+{
+
+
+  // get the number of bins
+  
+
+}                                                
--- CRUNCH_bins.hpp
+++ CRUNCH_bins.hpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <vector>
 #include "CRN_tParticle_bins.hpp"
+#include "VolumeParticleInfo.hpp"
+#include "CRN_tParticle_bins.hpp"
 using namespace std;
 
 #ifndef CRUNCH_bins_H
@@ -15,12 +17,40 @@ using namespace std;
 class CRUNCH_bins
 {
 	public:
-	  CRUNCH_binss()			{ create(); }
+	  /// @brief the default constructor which doens't do anything
+	  CRUNCH_bins()			{ create(); }
+	  
+	  /// @brief constructor just initialises the volume particle info
+	  /// object, and the number of cells
+	  /// @author SMM
+	  /// @date 07/08/2014
+	  CRUNCH_bins( int n_pdz, int n_caz, VolumeParticleInfo start_vpi)
+	                    { create(n_pdz, n_caz, start_vpi); }
+
+    /// @brief the data is stored in vectors, you need to get the index into
+    /// the vector of from the bin and cell number
+    /// @author SMM
+    /// @date 07/08/2014
+    int retrieve_index_of_cell(int bin, int cell);
+    
+    /// @brief this gathers all the information from the CRN tParticle bin
+    /// object including the locations of the cell boundaries, 
+    /// and the relevant geochemical information
+    /// @author SMM
+    /// @date 07/08/2014
+    void populate_cells_with_geochemical_data_from_CRNtPb(CRN_tParticle_bins& CRN_tPb);
+        
 
   protected:
   
     /// The number of bins
     int n_bins;
+    
+    /// The number of particle types
+    int n_types
+    
+    /// the volume particle info
+    VolumeParticleInfo vpi;
     
     /// The number of PDZ cells
     int n_pdz_cells;
@@ -35,10 +65,13 @@ class CRUNCH_bins
     /// is associated with a specific data type. This data can be found
     /// using a series of strings 
     map< string, vector<double> > cell_data_map; 
-   
+    
+       
 
 	private:
 	  void create();
+	  
+	  void create(int n_pdz, int n_caz, VolumeParticleInfo this_vpi);
   
 };
 
