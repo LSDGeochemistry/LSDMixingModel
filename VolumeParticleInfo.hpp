5ddc0a8e48aa3181168199822c789b8a427c4d27
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2018-07-20 13:15:58 +0100

    Some edits to the volume particle info object

--- VolumeParticleInfo.hpp
+++ VolumeParticleInfo.hpp
@@ -1,3 +1,50 @@
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// VolumeParticleInfo
+// An object for keeping track of particle information for geochemical and sediment
+// tracing experiments
+//
+// An object within the University
+//  of Edinburgh Land Surface Dynamics group mixing model
+//  for exploring hillslope mixing and particle weathering
+//
+// Developed by:
+//  Simon M. Mudd
+//
+// Copyright (C) 2018 Simon M. Mudd 2018
+//
+// Developer can be contacted by simon.m.mudd _at_ ed.ac.uk
+//
+//    Simon Mudd
+//    University of Edinburgh
+//    School of GeoSciences
+//    Drummond Street
+//    Edinburgh, EH8 9XP
+//    Scotland
+//    United Kingdom
+//
+// This program is free software;
+// you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation;
+// either version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY;
+// without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the
+// GNU General Public License along with this program;
+// if not, write to:
+// Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301
+// USA
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -6,6 +53,8 @@ using namespace std;
 #ifndef VolumeParticleInfo_HPP
 #define VolumeParticleInfo_HPP
 
+/// This class holds information about particles so that computations don't
+/// need to be repeated for each particle
 class VolumeParticleInfo
 {
  public:
@@ -38,6 +87,7 @@ class VolumeParticleInfo
     /// @date 01/01/11      
     double return_surface_area(int Type_index, int GSDindex, double mass);
 
+    // getter functions
     int get_n_types()					{ return n_types; }
     int get_n_sizes()					{ return n_sizes; }
     double get_ParticleTargetMass()		{ return ParticleTargetMass; }
@@ -50,23 +100,22 @@ class VolumeParticleInfo
     int n_sizes;
     vector<int> type_index;
     vector<string> type_name;
-	vector<double> size_index;			// you get particle size by taking 2^(size index)
-	vector<double> type_mfracs;
-	vector<double> size_mfracs;
-	vector<double> densities;			// in kg/m^3
-	vector<double> lambdas;				// roughness, used in surface area calucaltion
-	double ParticleTargetMass;			// the target mass of individual particles when particles are
-										// created the inserter tries to get as close to this mass whilst still
-										// conserving mass
+    vector<double> size_index;			// you get particle size by taking 2^(size index)
+    vector<double> type_mfracs;
+    vector<double> size_mfracs;
+    vector<double> densities;			// in kg/m^3
+    vector<double> lambdas;				// roughness, used in surface area calucaltion
+    double ParticleTargetMass;			// the target mass of individual particles when particles are
+                       // created the inserter tries to get as close to this mass whilst still
+                       // conserving mass
 
 	double parent_fracs[20][20];		// this stores the parent fraction array. It
-										// is designed to hold 20 types and 20 particle
-										// size classes. This can be expanded later
+                       	// is designed to hold 20 types and 20 particle
+                       	// size classes. This can be expanded later
 	double surface_area_multiplier[20][20];
-										// this converts mass to surface area for a given
-										// mass needs to be given in kg and area will be
-										// returned in m^2
-
+                       // this converts mass to surface area for a given
+                       // mass needs to be given in kg and area will be
+                       // returned in m^2
 
     void create();
     void create(const char*);
